0b52e875dd92f407d51455f7670ca296e037a667
f8t4l3rr0r <f8t4l3rr0r@gmail.com>
2020-01-16 10:13:19 -0700

    Readded Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Import Components
-// import NavBar from './components/layout/NavBar';
+import NavBar from './components/layout/NavBar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
@@ -26,7 +26,7 @@ const App = () => {
         <AlertState>
           <Router>
             <Fragment>
-              {/* <NavBar /> */}
+              <NavBar />
               <div className='container'>
                 <Alerts />
                 <Switch>
